e5cabb28895189a5b2ee458875d2f29204aa2dc5
Seher Kanwal <Study&Die@7>
2022-03-01 10:28:46 +0500

    final

--- lib/screen.dart
+++ lib/screen.dart
@@ -19,53 +19,52 @@ class _SecondScreenState extends State<SecondScreen> {
   Future getImages() async {
     final image = await imagepicker.pickImage(
         source: ImageSource.gallery, imageQuality: 80);
-    if(image==null)
-      {
-        const AlertDialog(
-          title: CircularProgressIndicator(),
 
-        );
-      }
-    else if (image != null) {
+    if (image != null) {
       setState(() {
         final temp = File(image.path);
         this.image = temp;
       });
     } else {
-      showDialog(context: context, builder:(_)=> const AlertDialog(
-        title: Text("No image is selected"),
-      ),);
+      showDialog(
+        context: context,
+        builder: (_) => const AlertDialog(
+          title: Text("No image is selected"),
+        ),
+      );
     }
   }
 
   Future postData() async {
-    setState((){
-       CircularProgressIndicator();
-    });
-    var stream = http.ByteStream(image!.openRead());
-    var length= await image!.length();
-    //var uri= await http.MultipartRequest(Uri.parse('https://fakestoreapi.com//products'));
-    var uri= Uri.parse('https://fakestoreapi.com//products');
-    var request= http.MultipartRequest('POST', uri);
-    request.fields['title']= " Hell ";
-    var multiport= http.MultipartFile('image', stream, length);
-    request.files.add(multiport);
+    setState(() { const CircularProgressIndicator(); });
+    var stream = http.ByteStream(image!.openRead()); //reading byte by byte
+    var length = await image!.length();
+    var uri = Uri.parse('https://fakestoreapi.com//products'); //the url to which we have to send request
+    var request = http.MultipartRequest('POST', uri); //in place of uri, you can write api's link
+    request.fields['title'] = " Hell ";
+    var multiport = http.MultipartFile('image', stream, length);
+    //we passing in image a stream(which is acutlly a image but in byte form and lenght of that stream)
+    //https://newbedev.com/use-flutter-to-send-a-http-post-request-containing-an-image-to-a-flask-api#:~:text=Use%20Flutter%20to%20send%20a%20http%20Post-Request%20%28containing,like%20File%20image%3Bwith%20the%20result%20of%20that%20picker.
+    //go to that link to understand
+    request.files.add(multiport); //this will add to multipart
     var response = await request.send();
-    if(response.statusCode==200)
-      {
-        print('Image is upload');
-       return  showDialog(context: context, builder:(_)=> const AlertDialog(
+    if (response.statusCode == 200) {
+      print('Image is upload');
+      return showDialog(
+        context: context,
+        builder: (_) => const AlertDialog(
           title: Text("Image is uploaded"),
-        ),);
-      }
-    else{
+        ),
+      );
+    } else {
       print('Uploading is failed');
-     return showDialog(context: context, builder:(_)=> const AlertDialog(
-        title: Text("Uploaded is failed"),
-      ),);
+      return showDialog(
+        context: context,
+        builder: (_) => const AlertDialog(
+          title: Text("Uploaded is failed"),
+        ),
+      );
     }
-
-
   }
 
   @override
@@ -82,32 +81,35 @@ class _SecondScreenState extends State<SecondScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             GestureDetector(
-              onTap: (){
-               getImages();
+              onTap: () {
+                getImages();
               },
               child: Container(
                 child: image == null
-                    ? const Center(child: Text("pick Image",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.lightBlue,
-                  fontWeight: FontWeight.bold,
-                ),))
+                    ? const Center(
+                        child: Text(
+                        "pick Image",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.lightBlue,
+                         // fontWeight: FontWeight.bold,
+                        ),
+                      ))
                     : Center(
-                      child: Image.file(
-                        File(image!.path).absolute,
-                        height: 200,
-                        width: 200,
-                        fit: BoxFit.cover,
+                        child: Image.file(
+                          File(image!.path).absolute, //give  the path from the currently working directory
+                          height: 200,
+                          width: 200,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
               ),
             ),
             const SizedBox(
               height: 150,
             ),
             GestureDetector(
-              onTap: (){
+              onTap: () {
                 postData();
               },
               child: Container(
@@ -115,19 +117,19 @@ class _SecondScreenState extends State<SecondScreen> {
                 height: 60,
                 decoration: const BoxDecoration(
                   color: Colors.blue,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15)
-                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
                 ),
                 child: const Center(
-                  child: Text("Upload",style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),),
+                  child: Text(
+                    "Upload",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ),
-
           ],
         ),
       ),
